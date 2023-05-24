0674771f5e3165e1fb17128194b81657a639b007
HighestDev <kenneth.danso@amalitech.org>
2023-05-24 14:10:46 +0000

    added a blank attrubute to the image field, not all images will have an image

--- shops/models.py
+++ shops/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=300)
     slug = models.SlugField(max_length=300)
-    image = models.ImageField(upload_to="products/Y%m%d%")
+    image = models.ImageField(upload_to="products/Y%m%d%", blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
