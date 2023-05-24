b7b19c720fa8764cb8996c7c9d6987ac84331667
HighestDev <kenneth.danso@amalitech.org>
2023-05-24 14:26:42 +0000

    added a couple of codes in the view.py to allow user views all products and a single product

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request,category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(active=True)
+    context={'category':category,'categories':categories,'products':products}
+    return render(request, 'shops/product/list.html', context)
+
+def product_detail(request,id,slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request,'shops/product/details.html',{'product':product})
