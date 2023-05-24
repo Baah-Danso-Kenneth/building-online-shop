8e712fad969bb5019cfe553a7bc2fa2148e89fc5
HighestDev <kenneth.danso@amalitech.org>
2023-05-24 13:49:44 +0000

    register models in th admin.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -82,12 +82,13 @@ WSGI_APPLICATION = "myshop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.environ.get('DATABASE_NAME'),
-        "HOST": os.environ.get('DATABASE_HOST'),
-        "USERNAME": os.environ.get('DATABASE_USERNAME'),
-        "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
-        "PORT": os.environ.get('DATABASE_PORT')
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USERNAME"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT")
+
     }
 }
 
