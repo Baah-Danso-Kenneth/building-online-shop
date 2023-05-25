0c8727b4e54b9738b5cbb450a6eed273398aef6d
HighestDev <kenneth.danso@amalitech.org>
2023-05-25 10:45:32 +0000

    context_processors allows users to make something availble globally and does what we have added to our project

--- carts/views.py
+++ carts/views.py
@@ -24,7 +24,7 @@ def cart_remove(request,product_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form']= CartAddProduct(initial={
+        item['update_quantity_form'] = CartAddProduct(initial={
             'quantity':item['quantity'],
             'override':True
         })
