73ac3fef30bd038c6e79fb29a47e67e5cc0caa8b
HighestDev <kenneth.danso@amalitech.org>
2023-05-31 08:20:33 +0000

    creating a couponsmodel for handling discount price in project

--- myshop/settings.py
+++ myshop/settings.py
@@ -44,8 +44,10 @@ INSTALLED_APPS = [
     'shops.apps.ShopsConfig',
     'carts.apps.CartsConfig',
     'orders.apps.OrdersConfig',
-    'payments.apps.PaymentsConfig'
+    'payments.apps.PaymentsConfig',
+    'coupons.apps.CouponsConfig',
 ]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
