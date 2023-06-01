da28bc27552ca84252dec0dd5490dbeda652606b
HighestDev <kenneth.danso@amalitech.org>
2023-06-01 10:06:36 +0000

    sum up the Stripe payment

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,7 @@ from shops.models import Product
 from django.views.decorators.http import require_POST
 from .cart import Cart
 from .forms import CartAddProduct
-
+from coupons.forms import CouponApplyForm
 @require_POST
 def cart_add(request,product_id):
     cart = Cart(request)
@@ -28,4 +28,5 @@ def cart_detail(request):
             'quantity':item['quantity'],
             'override':True
         })
-    return render(request,'carts/detail.html',{'cart':cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request,'carts/detail.html',{'cart':cart, 'coupon_apply_form':coupon_apply_form})
--- myshop/settings.py
+++ myshop/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 CART_SESSION_ID = os.environ.get('CART_SESSION_ID')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
